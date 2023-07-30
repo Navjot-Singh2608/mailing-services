04add4a49a30cf431bbad5a20a6947cd45e5d7f0
navjotsingh2608 <navjot@argenelec.com>
2023-07-30 00:35:33 -0400

    update

---
+++ src/main/java/com/banking/mailingservices/MailingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.banking.mailingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailingServicesApplication.class, args);
+	}
+
+}
